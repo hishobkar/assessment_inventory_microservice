b649724b820a0a95442c44d70e790cb8583fddff
Abhijeet <abhishobkar@gmail.com>
2025-03-16 18:33:18 +0530

    Pre database migration project

---
+++ InventoryManagement/OrderService/Data/OrderDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrderService.Models;
+
+namespace OrderService.Data
+{
+    public class OrderDbContext : DbContext
+    {
+        public OrderDbContext(DbContextOptions<OrderDbContext> options) : base(options) { }
+        DbSet<Order> orders {  get; set; }
+    }
+}
---
+++ InventoryManagement/ProductService/Data/ProductDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductService.Models;
+
+namespace ProductService.Data
+{
+    public class ProductDbContext : DbContext
+    {
+        public ProductDbContext(DbContextOptions<ProductDbContext> options) : base(options)
+        {   
+        }
+
+        DbSet<Product> products { get; set; }
+    }
+}
